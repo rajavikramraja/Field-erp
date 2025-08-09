62fd9097999388c8ecc83e1b7b530114c8d64e04
cheyanv <cheyanv@gmail.com>
2025-08-09 19:27:41 +0530

    Login Issue Recitified

--- src/test/java/org/erp/Login.java
+++ src/test/java/org/erp/Login.java
@@ -1,10 +1,18 @@
 package org.erp;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
 import org.utilize.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -15,20 +23,22 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Login {
 	WebDriver driver;
+	WebDriverWait wait;
+	
 @BeforeMethod
 public void lauchbrowser() {
 	WebDriverManager.chromedriver().setup();
 	 driver=new ChromeDriver();
 	driver.get("https://trustedtools.in/field-erp/admin/auth/login");
-	System.out.println("Hi");
-	System.out.println("Hello");
-	
+	//System.out.println("Hi");
+	wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
 }
 @DataProvider(name="logindata")
 public Object[][] logindaObjects() throws IOException {
 	return ParentClass.getexcel("C:\\Users\\vikra\\OneDrive\\Documents\\erppassword.xlsx", "Sheet1");	
 }
-@Test(dataProvider = "logindata")
+@Test(dataProvider ="logindata")
 public void login(String username, String Password) {
 	driver.findElement(By.id("name")).sendKeys(username);
 	driver.findElement(By.id("password")).sendKeys(Password);
@@ -36,12 +46,38 @@ public void login(String username, String Password) {
 	
 	String expectedurl="https://trustedtools.in/field-erp/admin/dashboard";
 	if (username.equals("vikram-kitchens")&& Password.equals("test@123")) {
+		Boolean until = wait.until(ExpectedConditions.urlContains(expectedurl));
+		System.out.println(driver.getCurrentUrl() );
 	assert	driver.getCurrentUrl().equals(expectedurl);
 	System.out.println("Login Succefull" + username);
 	} else {
-boolean displayed = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).isDisplayed();
-assert displayed;
-System.out.println("Login valid"+ username);
+//boolean displayed = driver.findElement(By.xpath("//p[normalize-space()='The Username field is required.']")).isDisplayed();
+//assert displayed;
+//System.out.println("Login valid"+ username);
+		
+		
+		WebElement errorElement = wait.until(
+			    ExpectedConditions.visibilityOfElementLocated(
+			        By.xpath("//p[normalize-space()='The Username field is required.']"+
+			    "| //p[normalize-space()='The Password field is required.']" 
+			        		+"| //div[@class='alert alert-danger']")
+			    )
+			);
+
+		String errorText = errorElement.getText() .replaceAll("[^\\x20-\\x7E]", "")  // remove non-ASCII printable characters
+			    .replaceAll("\\s+", " ")  .trim();
+		System.out.println("Error Message: " + errorText);
+
+		// List of expected messages
+		List<String> expectedMessages = Arrays.asList(
+		    "The Username field is required.",
+		    "The Password field is required.",
+		    "The Password field is required, The Username field is required.",
+		    "Invalid Username or Password!"
+		);
+
+		assert expectedMessages.contains(errorText) : "Unexpected error message: " + errorText;
+
 
 	}
 }
--- src/test/java/org/utilize/ParentClass.java
+++ src/test/java/org/utilize/ParentClass.java
@@ -3,8 +3,11 @@ package org.utilize;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -45,47 +48,114 @@ public static void screenshot(String loc) throws IOException {
 	File file =new File(loc);
 	FileUtils.copyFile(screenshotAs, file);
 }
+//public static Object[][] getexcel(String loc, String sheet) throws IOException {
+//FileInputStream files=new FileInputStream(loc);
+//XSSFWorkbook xssfWorkbook =new XSSFWorkbook();
+//XSSFSheet sheets = xssfWorkbook.getSheet(sheet);
+//int no_of_row = sheets.getPhysicalNumberOfRows();
+//int no_of_col = sheets.getRow(0).getPhysicalNumberOfCells();
+//Object[][] obj= new Object[no_of_row-1][no_of_col];
+//for (int i= 1; i < no_of_row; i++) {
+//	XSSFRow row = sheets.getRow(i);
+//	for (int j = 0; j < no_of_col; j++) {
+//		XSSFCell cell = row.getCell(j);
+//		if (cell==null) {
+//		obj[i-1][0]="";
+//		continue;
+//		}
+//		switch (cell.getCellType()) {
+//		case STRING:
+//			obj[i-1][j]=cell.getStringCellValue();
+//			break;
+//		case NUMERIC:
+//			if (DateUtil.isCellDateFormatted(cell)) {
+//				obj[i-1][j]=String.valueOf(cell.getDateCellValue());
+//			} else {
+//				obj[i-1][j]=String.valueOf(cell.getNumericCellValue());
+//			}
+//		break;
+//		case BOOLEAN:
+//			obj[i-1][j]=String.valueOf(cell.getBooleanCellValue());
+//			break;
+//		case FORMULA:
+//			obj[i-1][j]=String.valueOf(cell.getCellFormula());
+//			break;
+//		default:
+//
+//			obj[i-1][j]="";
+//			break;
+//		}
+//	}
+//}
+//xssfWorkbook.close();
+//files.close();
+//return obj;
+//}
 public static Object[][] getexcel(String loc, String sheet) throws IOException {
-FileInputStream files=new FileInputStream(loc);
-XSSFWorkbook xssfWorkbook =new XSSFWorkbook();
-XSSFSheet sheets = xssfWorkbook.getSheet(sheet);
-int no_of_row = sheets.getPhysicalNumberOfRows();
-int no_of_col = sheets.getRow(0).getPhysicalNumberOfCells();
-Object[][] obj= new Object[no_of_row-1][no_of_col];
-for (int i= 1; i < no_of_row; i++) {
-	XSSFRow row = sheets.getRow(i);
-	for (int j = 0; j < no_of_col; j++) {
-		XSSFCell cell = row.getCell(j);
-		if (cell==null) {
-		obj[i-1][0]="";
-		continue;
-		}
-		switch (cell.getCellType()) {
-		case STRING:
-			obj[i-1][j]=cell.getStringCellValue();
-			break;
-		case NUMERIC:
-			if (DateUtil.isCellDateFormatted(cell)) {
-				obj[i-1][j]=String.valueOf(cell.getDateCellValue());
-			} else {
-				obj[i-1][j]=String.valueOf(cell.getNumericCellValue());
-			}
-		break;
-		case BOOLEAN:
-			obj[i-1][j]=String.valueOf(cell.getBooleanCellValue());
-			break;
-		case FORMULA:
-			obj[i-1][j]=String.valueOf(cell.getCellFormula());
-			break;
-		default:
+    FileInputStream files = new FileInputStream(loc);                 // 1
+    XSSFWorkbook workbook = new XSSFWorkbook(files);                  // 2
+    XSSFSheet sheets = workbook.getSheet(sheet);                      // 3
 
-			obj[i-1][j]="";
-			break;
-		}
-	}
-}
-xssfWorkbook.close();
-files.close();
-return obj;
+    int no_of_row = sheets.getPhysicalNumberOfRows();                 // 4
+    int no_of_col = sheets.getRow(0).getPhysicalNumberOfCells();      // 5
+
+    List<Object[]> dataList = new ArrayList();                      // 6
+
+    for (int i = 1; i < no_of_row; i++) {                             // 7 (start from 1 = skip header)
+        XSSFRow row = sheets.getRow(i);                               // 8
+        if (row == null) continue;                                   // 9
+
+        Object[] rowData = new Object[no_of_col];                    // 10
+        boolean allEmpty = true;                                     // 11
+
+        for (int j = 0; j < no_of_col; j++) {                        // 12
+            XSSFCell cell = row.getCell(j);                          // 13
+            String cellValue = "";                                   // 14
+
+            if (cell == null || cell.getCellType() == CellType.BLANK) {
+                cellValue = "";
+            } else {
+                switch (cell.getCellType()) {                        // 15
+                    case STRING:
+                        cellValue = cell.getStringCellValue().trim();
+                        break;
+                    case NUMERIC:
+                        if (DateUtil.isCellDateFormatted(cell)) {
+                            cellValue = String.valueOf(cell.getDateCellValue());
+                        } else {
+                            cellValue = String.valueOf((long) cell.getNumericCellValue());
+                        }
+                        break;
+                    case BOOLEAN:
+                        cellValue = String.valueOf(cell.getBooleanCellValue());
+                        break;
+                    case FORMULA:
+                        cellValue = cell.getCellFormula();
+                        break;
+                    default:
+                        cellValue = cell.toString().trim();
+                }
+            }
+
+            if (!cellValue.isEmpty()) {                             // 16
+                allEmpty = false;
+            }
+            rowData[j] = cellValue;                                 // 17
+        }
+
+        if (!allEmpty) {                                             // 18
+            dataList.add(rowData);
+        }
+    }
+
+    workbook.close();                                                // 19
+    files.close();                                                   // 20
+
+    // Convert List<Object[]> to Object[][]
+    Object[][] obj = new Object[dataList.size()][no_of_col];         // 21
+    for (int i = 0; i < dataList.size(); i++) {                      // 22
+        obj[i] = dataList.get(i);                                    // 23
+    }
+    return obj;                                                      // 24
 }
 }
